fdc3c0104b8a67b28f145fce1348317d9510c414
kiorisyshen <kiorisyshen@gmail.com>
2020-09-05 13:37:20 +0900

    End ch09

---
+++ src/ch09-03-to-panic-or-not-to-panic/src/main.rs
@@ -0,0 +1,42 @@
+fn main() {
+    let secret_number = 30;
+
+    // Here we may check guess in all functions
+    // Use Guess struct instead for safty
+    loop {
+        // --snip--
+        let guess = "101";
+        let guess: i32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        if guess < 1 || guess > 100 {
+            println!("The secret number will be between 1 and 100.");
+            continue;
+        }
+
+        match guess.cmp(&secret_number) {
+            _ => println!(""),
+        }
+        // --snip--
+    }
+}
+
+pub struct Guess {
+    value: i32,
+}
+
+impl Guess {
+    pub fn new(value: i32) -> Guess {
+        if value < 1 || value > 100 {
+            panic!("Guess value must be between 1 and 100, got {}.", value);
+        }
+
+        Guess { value }
+    }
+
+    pub fn value(&self) -> i32 {
+        self.value
+    }
+}
