4bbb49439b9c999d4788b414ccf9fc560836eb80
kiorisyshen <kiorisyshen@gmail.com>
2020-09-11 10:10:58 +0900

    Start ch15

---
+++ src/ch15-02-deref/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    let x = 5;
+    let y = &x;
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+
+    // like a reference
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
+}
