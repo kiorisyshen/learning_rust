c9d7d2525f9d0a0f8f6dc137da25b8ec44274de1
kiorisyshen <kiorisyshen@gmail.com>
2020-09-21 16:10:14 +0900

    End ch18

---
+++ src/ch18-02-refutability/src/main.rs
@@ -0,0 +1,9 @@
+fn main() {
+    // some_option_value might be None
+    // let Some(x) = some_option_value;
+
+    // The pattern will always match, will have warning
+    // if let x = 5 {
+    //     println!("{}", x);
+    // };
+}
